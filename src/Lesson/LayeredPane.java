bdc5a59f4bf792a39986bc6773953c900e2ec3dd
ilyaKhim <47591146+ilyaKhim@users.noreply.github.com>
2019-03-18 16:27:12 +0600

    Update LayeredPane.java

--- src/Lesson/LayeredPane.java
+++ src/Lesson/LayeredPane.java
@@ -1,13 +1,45 @@
 package Lesson;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class LayeredPane {
+
+public class LayeredPane extends JFrame {
     public LayeredPane() {
         //создание окна
-        JFrame frame = new JFrame("Example LayeredTest");
+        super("Example");
 
         // выход при закрытии окна
-        JFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //определение многослойной панели
+        JLayeredPane lp = getLayeredPane();
+
+        // создание 3х фигур
+        Figure figure1 = new Figure(Color.red, 0, "Figure PopUp");
+        Figure figure2 = new Figure(Color.blue, 0, "Figure 1") ;
+        Figure figure3 = new Figure(Color.cyan, 1, "Figure 2");
+
+        // определение местоположения фигур в окне
+        figure1.setBounds(10, 40,120, 120 );
+        figure2.setBounds(60, 120, 160, 180);
+        figure3.setBounds(90, 55, 250, 180);
+
+        // добавление фигур в различные слои
+        lp.add(figure1, JLayeredPane.POPUP_LAYER);
+        lp.add(figure2, JLayeredPane.PALETTE_LAYER);
+        lp.add(figure3, JLayeredPane.PALETTE_LAYER);
+
+        //смена позиций 1й из фигур
+        //lp.setPosition(figure3, 0);
+
+        // определение размера окна и его открытие
+        setSize(280, 250);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        new LayeredPane();
     }
 }
